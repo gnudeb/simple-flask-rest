82f74ee9d0cd8b2f674736958ad7f5745d9d57c8
Alan Watts <gnudeb0@gmail.com>
2017-11-04 14:17:50 +0200

    Add handling of invalid incoming JSON

--- app/api.py
+++ app/api.py
@@ -4,31 +4,40 @@ from app import app
 from app import models
 import json
 from json.decoder import JSONDecodeError
+from jsonschema.exceptions import ValidationError
 
 api = Api(app)
 api_root = "/api"
 
 class UserRegistration(Resource):
-    def put(self):
+
+    def post(self):
         response = Response(mimetype="application/json")
         raw_json = request.form["data"]
+
         try:
             user = models.User.from_json(raw_json)
         except models.User.UserAlreadyExistsError:
             response.status = "409"
-            response.response = json.dumps({
+            response.data = json.dumps({
                 "code": "USER_ALREADY_EXISTS",
                 "description": "A user with specified username already exists"
             })
         except JSONDecodeError:
             response.status = "400"
-            response.response = json.dumps({
+            response.data = json.dumps({
                 "code": "INVALID_JSON",
                 "description": "Provided JSON is invalid"
             })
+        except ValidationError:
+            response.status = "400"
+            response.data = json.dumps({
+                "code": "INVALID_JSON_DATA",
+                "description": "Provided JSON does not contain required data"
+            })
         else:
             response.status = "200"
-            response.response = user.to_json()
+            response.data = user.to_json()
             models.User.create_user(user)
 
         return response
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 import json
+import jsonschema
 
 db = SQLAlchemy(app)
 
@@ -12,16 +13,40 @@ class User(db.Model):
     plain_password = db.Column(db.String(64))
     hashed_password = db.Column(db.String(128))
 
+    json_schema = {
+            "type": "object",
+            "properties": {
+                "firstName": {"type": "string"},
+                "lastName": {"type": "string"},
+                "userName": {"type": "string"},
+                "password": {"type": "string"},
+            },
+            "required": [
+                "firstName",
+                "lastName",
+                "userName",
+                "password",
+            ],
+    }
+
+    def __init__(self, first_name, last_name, user_name, plain_password):
+        if User.exists(user_name):
+            raise User.UserAlreadyExistsError
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.plain_password = plain_password
+        self.id = len(User.query.all())
+
     def exists(user_name):
         return User.query.filter_by(user_name=user_name).first() is not None
 
     def from_json(raw_json):
         data = json.loads(raw_json)
-        if User.exists(data["userName"]):
-            raise User.UserAlreadyExistsError
-
+        jsonschema.validate(data, User.json_schema)
+        
         user = User(
-                id=len(User.query.all()),
                 first_name=data["firstName"],
                 last_name=data["lastName"],
                 user_name=data["userName"],
@@ -35,13 +60,16 @@ class User(db.Model):
         db.session.commit()
 
     def to_json(self):
-        json_object = {
+        return json.dumps(self.to_dict())
+
+    def to_dict(self):
+        return {
                 "id": self.id,
                 "firstName": self.first_name,
                 "lastName": self.last_name,
                 "userName": self.user_name
         }
-        return json.dumps(json_object)
+
 
     class UserAlreadyExistsError(Exception):
         pass
